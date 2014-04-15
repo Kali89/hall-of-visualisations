f8fc99f6560475eada7eafc7a0a329f8d142ee61
Kali89 <matthewsharpe3@gmail.com>
2014-04-15 21:01:30 +0100

    Refactored and added additional functionality

--- app.py
+++ app.py
@@ -22,10 +22,56 @@ def average_price(year):
 			data_dictionary[address_7.strip('"').strip().lower()] = [int(price.strip('"'))]
 	final_results = dict((lookup_dictionary[key], sum(value)/float(len(value))) for key,value in data_dictionary.iteritems() if key in lookup_dictionary)
 	return final_results
+	
+def number_of_new_builds(year):
+	with open('ISOCodes.txt', 'rb') as f:
+		lookup_dictionary = dict((line.split(':')[1].strip('\n').strip().lower(), line.split(':')[0].strip('\n').strip()) for line in f.readlines())
+	data_dictionary = {}
+	with open('pp-' + str(year) + '.csv', 'rb') as f:
+		lines = f.readlines()[1:]
+	for line in lines:
+		try:
+			identifier,price,date,postcode,type_of_house,new,freehold_or_leasehold,address_1,address_2,address_3,address_4,address_5,address_6,address_7,letter = line.strip().strip('\n').split(',')
+		except:
+			continue
+		new_value = 1 if new.strip('"') == "N" else 0
+		try:
+			data_dictionary[address_7.strip('"').strip().lower()].append(new_value)
+		except:
+			data_dictionary[address_7.strip('"').strip().lower()] = [new_value]
+	final_results = dict((lookup_dictionary[key], sum(value)) for key,value in data_dictionary.iteritems() if key in lookup_dictionary)
+	return final_results
+	
+def best_selling_month(year):
+	with open('ISOCodes.txt', 'rb') as f:
+		lookup_dictionary = dict((line.split(':')[1].strip('\n').strip().lower(), line.split(':')[0].strip('\n').strip()) for line in f.readlines())
+	data_dictionary = {}
+	with open('pp-' + str(year) + '.csv', 'rb') as f:
+		lines = f.readlines()[1:]
+	for line in lines:
+		try:
+			identifier,price,date,postcode,type_of_house,new,freehold_or_leasehold,address_1,address_2,address_3,address_4,address_5,address_6,address_7,letter = line.strip().strip('\n').split(',')
+		except:
+			continue
+		month = int(date.strip('"').split('-')[1])
+		try:
+			data_dictionary[address_7.strip('"').strip().lower()][month].append(int(price.strip('"')))
+		except:
+			try:
+				data_dictionary[address_7.strip('"').strip().lower()][month] = [int(price.strip('"'))]
+			except:
+				data_dictionary[address_7.strip('"').strip().lower()] = {month : [int(price.strip('"'))]}
+
+		for region in data_dictionary.keys():
+			max_checker = [(index,len(thing)) for index, thing in enumerate(data_dictionary[region].keys())]
+			## Get best month for sales and for revenue
+				
+	return final_results
+	
 			
 class slider_test:
     def GET(self):
-		result_dictionary = average_price(2014)
+		result_dictionary = number_of_new_builds(2014)
 		return render.slider_test(result_dictionary)
          
     def POST(self):
